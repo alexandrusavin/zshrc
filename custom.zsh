58b3f61a2edef7dab7e0ecce9938fcdb2d037192
Alexandru Savin <alexandrusavin@gmail.com>
2017-02-27 23:37:05 +0100

    First commit

--- custom.zsh
+++ custom.zsh
@@ -1,6 +1,3 @@
-# Path to your oh-my-zsh installation.
-export ZSH=/Users/alex/.oh-my-zsh
-
 # Set name of the theme to load.
 # Look in ~/.oh-my-zsh/themes/
 # Optionally, if you set this to "random", it'll load a random theme each
@@ -59,9 +56,6 @@ plugins=(zsh-nvm git colored-man colorize github brew osx zsh-syntax-highlightin
 # User configuration
 
 export PATH="/usr/local:/usr/local:/usr/bin:/bin:/usr/sbin:/sbin"
-# export MANPATH="/usr/local/man:$MANPATH"
-
-source $ZSH/oh-my-zsh.sh
 
 # You may need to manually set your language environment
 # export LANG=en_US.UTF-8
